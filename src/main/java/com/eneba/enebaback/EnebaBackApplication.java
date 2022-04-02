191824c0245c8d44608300fde34547d18adcc006
Arijus Kliševičius <justgod153@live.com>
2022-04-02 15:20:59 +0300

    Initial commit

---
+++ src/main/java/com/eneba/enebaback/EnebaBackApplication.java
@@ -0,0 +1,13 @@
+package com.eneba.enebaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnebaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnebaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eneba/enebaback/EnebaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eneba.enebaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnebaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
