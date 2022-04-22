ffc658607e55d2fc3be48a3652ec5cd59160d92d
zyke-99 <zyke-99>
2022-04-22 18:25:12 +0300

    added jwt-based authentication and authorization:
    authentication done via /auth post request - returns jwt token
    authorization is done when accessing any other endpoints, expects jwt token from header (key: Authorization, value: Bearer <token>

--- src/main/java/com/eneba/enebaback/EnebaBackApplication.java
+++ src/main/java/com/eneba/enebaback/EnebaBackApplication.java
@@ -2,12 +2,22 @@ package com.eneba.enebaback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+//TODO remove exclude setting once db is set up
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class EnebaBackApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EnebaBackApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
---
+++ src/main/java/com/eneba/enebaback/dto/jwt/JwtRequest.java
@@ -0,0 +1,15 @@
+package com.eneba.enebaback.dto.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtRequest {
+
+    private String email;
+    private String password;
+
+}
