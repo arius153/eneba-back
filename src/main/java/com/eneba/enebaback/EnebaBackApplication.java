67e12affc78f0503e6f845f760c5688e35a5166a
Arijus Kliševičius <justgod153@live.com>
2022-05-03 17:27:57 +0300

    prijungta duombazė

--- src/main/java/com/eneba/enebaback/EnebaBackApplication.java
+++ src/main/java/com/eneba/enebaback/EnebaBackApplication.java
@@ -2,13 +2,11 @@ package com.eneba.enebaback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-//TODO remove exclude setting once db is set up
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class EnebaBackApplication {
 
     public static void main(String[] args) {
