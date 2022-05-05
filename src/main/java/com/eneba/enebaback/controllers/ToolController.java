a22c4d63e84a495246ed45a9624caa99c83ffc94
Arijus Kliševičius <justgod153@live.com>
2022-05-05 11:39:42 +0300

    Tool pridėjimas į duomenų bazę

--- src/main/java/com/eneba/enebaback/controllers/ToolController.java
+++ src/main/java/com/eneba/enebaback/controllers/ToolController.java
@@ -1,24 +1,25 @@
 package com.eneba.enebaback.controllers;
 
 import com.eneba.enebaback.dto.CategoryDTO;
+import com.eneba.enebaback.dto.ToolDTO;
 import com.eneba.enebaback.dto.ToolRegisterDTO;
-import com.eneba.enebaback.services.ToolServiceImpl;
+import com.eneba.enebaback.services.impl.ToolServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.eneba.enebaback.dto.ToolDTO;
-import com.eneba.enebaback.services.ToolService;
->>>>>>> origin/master
 
 @RestController
 @RequestMapping("/tool")
@@ -34,11 +35,8 @@ public class ToolController {
 
     @PostMapping
     public Long addTool(@RequestPart("data") ToolRegisterDTO toolRegisterDTO, @RequestPart(value = "files", required = false) List<MultipartFile> files) {
-        toolService.registerTool(toolRegisterDTO, files);
+        return toolService.registerTool(toolRegisterDTO, files);
     }
-
-    @Autowired
-    private ToolService toolService;
 
     @GetMapping("/all")
     public List<ToolDTO> getAllTools() {
--- src/main/java/com/eneba/enebaback/services/UserServiceImpl.java
+++ src/main/java/com/eneba/enebaback/services/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.eneba.enebaback.services;
 
+import com.eneba.enebaback.dto.CustomUser;
 import com.eneba.enebaback.dto.UserRegisterDTO;
 import com.eneba.enebaback.entities.User;
 import com.eneba.enebaback.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserDetailsService {
@@ -22,19 +26,29 @@ public class UserServiceImpl implements UserDetailsService {
     PasswordEncoder bCryptPasswordEncoder;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public CustomUser loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(username);
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+        return new CustomUser(user.getEmail(), user.getPassword(), new ArrayList<>(), user.getId()) {
+        };
     }
 
     public User RegisterUser(UserRegisterDTO userRegisterDTO) {
-
         User user = new User();
         user.setEmail(userRegisterDTO.getEmail());
         user.setPassword(bCryptPasswordEncoder.encode(userRegisterDTO.getPassword()));
         user.setName(userRegisterDTO.getName());
         user.setSurname(userRegisterDTO.getSurname());
         return userRepository.save(user);
+    }
 
+    public Long getLoggedUserId() {
+        var securityContext = SecurityContextHolder.getContext();
+        if (securityContext != null) {
+            var details = securityContext.getAuthentication().getPrincipal();
+            if (details instanceof CustomUser) {
+                return ((CustomUser) details).getId();
+            }
+        }
+        return null;
     }
 }
