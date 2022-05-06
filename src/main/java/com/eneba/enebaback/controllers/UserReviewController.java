e3720f19083c144032f6ece3ea4bac92655853bb
zyke-99 <zyke-99>
2022-05-06 20:44:19 +0300

    added functionality to retrieve user reviews by user id

--- src/main/java/com/eneba/enebaback/controllers/UserReviewController.java
+++ src/main/java/com/eneba/enebaback/controllers/UserReviewController.java
@@ -2,6 +2,7 @@ package com.eneba.enebaback.controllers;
 
 import com.eneba.enebaback.dto.UserReviewDTO;
 import com.eneba.enebaback.services.UserReviewServiceImpl;
+import com.eneba.enebaback.services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +18,12 @@ public class UserReviewController {
     @Autowired
     private UserReviewServiceImpl userReviewService;
 
+    @Autowired
+    private UserServiceImpl userService;
+
     @GetMapping
-    public List<UserReviewDTO> getUserReviewsById(@RequestParam Long id) {
-        return userReviewService.getUserReviewsByUserId(id);
+    public List<UserReviewDTO> getUserReviewsById() {
+        return userReviewService.getUserReviewsByUserId(userService.getLoggedUserId());
     }
 
 }
